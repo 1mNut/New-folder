206d3b8cde9999e04a43015a41ff6d9ba2018b2f
1mNut <sebastian.nilsson@skola.taby.se>
2024-11-15 09:58:21 +0100

    lektion 1

--- player.py
+++ player.py
@@ -1,8 +1,11 @@
 class Player:
-    def __innit__(self, name, Health, Strength, Level):
+    def __init__(self, name, Health, Strength, Level):
         self.name = name
         self.Health = Health
         self.Strength = Strength
         self.Level = Level
-    def print_person_info(self):
-        print(self.name, self.Health, self.Strength, self.Level)
+    def show_stats(self):
+        print(f"Name -> {self.name}")
+        print(f"Health -> {self.Health}")
+        print(f"Strength -> {self.Strength}")
+        print(f"Level -> {self.Level}")
--- spel.py
+++ spel.py
@@ -1,2 +1,28 @@
+import random 
 from player import Player
-name = input("Välj din gubbes namn ->")
+
+name = input("Välj karaktärens namn -> ")
+player = Player(name, 1, 1, 1)
+
+def choose_door():
+    print("Choose a door:")
+    print("1. Door 1")
+    print("2. Door 2")
+    print("3. Door 3")
+    while True:
+        choice = input("Enter your choice (1-3): ")
+        if choice in ['1', '2', '3']:
+            return int(choice)
+        else:
+            print("Invalid choice. Please enter 1, 2, or 3.")
+
+while True:
+    Val = input("Dörr (1) eller inventory (2)")
+    if Val == "1":
+        choose_door()
+        break
+    elif Val == "2":
+        player.show_stats()
+        break
+    else:
+        print("invalid choice, choose: 1 or 2")
